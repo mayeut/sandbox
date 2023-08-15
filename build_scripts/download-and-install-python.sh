19ff7cc8579fb6a3c11f49c3b6a4abc0e4f3d423
mayeut <mayeut@users.noreply.github.com>
2023-08-15 14:41:17 +0200

    Update download-and-install-python.sh

--- build_scripts/download-and-install-python.sh
+++ build_scripts/download-and-install-python.sh
@@ -10,7 +10,6 @@ ABI_TAG=$1
 DOWNLOAD_URL=$2
 SHA256=$3
 
-TMPDIR=/tmp/manylinux-download
 PREFIX="/opt/_internal/${ABI_TAG}"
 
 case ${DOWNLOAD_URL} in
@@ -23,14 +22,9 @@ esac
 
 mkdir ${PREFIX}
 
-mkdir -p ${TMPDIR}
-
-echo "${SHA256} -" > ${TMPDIR}/sha256
-time curl -fsSL ${DOWNLOAD_URL} | tee >(tar -C ${PREFIX} --strip-components 1 -x${COMP}f -) | sha256sum -c ${TMPDIR}/sha256
+time curl -fsSL ${DOWNLOAD_URL} | tee >(tar -C ${PREFIX} --strip-components 1 -x${COMP}f -) | sha256sum -c <(echo "${SHA256} -")
 
 # remove debug symbols if any
 time find ${PREFIX}/bin -name '*.debug' -delete
-
-rm -rf ${TMPDIR}
 
 time ${MY_DIR}/finalize-one.sh ${PREFIX}
