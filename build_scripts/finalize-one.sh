c16c710d56cb7135e7f8ff8efcc5a67ead17f0fd
mayeut <mayeut@users.noreply.github.com>
2023-08-15 13:05:50 +0200

    Update finalize-one.sh

--- build_scripts/finalize-one.sh
+++ build_scripts/finalize-one.sh
@@ -13,15 +13,17 @@ MY_DIR=$(dirname "${BASH_SOURCE[0]}")
 if [ -e ${PREFIX}/bin/python3 ] && [ ! -e ${PREFIX}/bin/python ]; then
 	ln -s python3 ${PREFIX}/bin/python
 fi
-time ${PREFIX}/bin/python -m ensurepip
-if [ -e ${PREFIX}/bin/pip3 ] && [ ! -e ${PREFIX}/bin/pip ]; then
-	ln -s pip3 ${PREFIX}/bin/pip
+if ! ${PREFIX}/bin/python -m pip -V; then
+	time ${PREFIX}/bin/python -m ensurepip
 fi
 PY_VER=$(${PREFIX}/bin/python -c "import sys; print('.'.join(str(v) for v in sys.version_info[:2]))")
 PY_IMPL=$(${PREFIX}/bin/python -c "import sys; print(sys.implementation.name)")
 # Since we fall back on a canned copy of pip, we might not have
 # the latest pip and friends. Upgrade them to make sure.
 time ${PREFIX}/bin/python -m pip install -U --require-hashes -r ${MY_DIR}/requirements${PY_VER}.txt
+if [ -e ${PREFIX}/bin/pip3 ] && [ ! -e ${PREFIX}/bin/pip ]; then
+	ln -s pip3 ${PREFIX}/bin/pip
+fi
 # Create a symlink to PREFIX using the ABI_TAG in /opt/python/
 ABI_TAG=$(${PREFIX}/bin/python ${MY_DIR}/python-tag-abi-tag.py)
 ln -s ${PREFIX} /opt/python/${ABI_TAG}
